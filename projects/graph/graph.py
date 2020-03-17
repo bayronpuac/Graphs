70e5479306878bfc9b4f3d01dccf9074a9ddc62f
Bayron Puac <bayronpuac@gmail.com>
2020-03-17 11:56:39 -0500

    Updated Graph again

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -189,7 +189,7 @@ class Graph:
                     # PUSH THE COPY
                     s.push(path_copy)
 
-    def dfs_recursive(self, starting_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
